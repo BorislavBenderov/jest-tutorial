2fdd229f02aebb461f6a574064f5e975f1b4e0f2
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-21 15:19:32 +0200

    Test if count should be 0 if the restart button is clicked

--- src/Counter.test.js
+++ src/Counter.test.js
@@ -7,6 +7,7 @@ describe(Counter, () => {
     const countValue = Number(getByTestId('count').textContent);
     expect(countValue).toEqual(0);
   });
+
   it('count should increment by 1 if the increment button is clicked', () => {
     const { getByTestId, getByRole } = render(<Counter initialCount={0} />);
     const incrementBtn = getByRole('button', { name: 'Increment' });
@@ -16,6 +17,7 @@ describe(Counter, () => {
     const countValue2 = Number(getByTestId('count').textContent);
     expect(countValue2).toEqual(1);
   });
+
   it('count should decrement by 1 if the decrement button is clicked', () => {
     const { getByTestId, getByRole } = render(<Counter initialCount={0} />);
     const decrementBtn = getByRole('button', { name: 'Decrement' });
@@ -24,5 +26,15 @@ describe(Counter, () => {
     fireEvent.click(decrementBtn);
     const countValue2 = Number(getByTestId('count').textContent);
     expect(countValue2).toEqual(-1);
+  });
+
+  it('count should be 0 if the restart button is clicked', () => {
+    const { getByTestId, getByRole } = render(<Counter initialCount={50} />);
+    const restartBtn = getByRole('button', { name: 'Restart' });
+    const countValue1 = Number(getByTestId('count').textContent);
+    expect(countValue1).toEqual(50);
+    fireEvent.click(restartBtn);
+    const countValue2 = Number(getByTestId('count').textContent);
+    expect(countValue2).toEqual(0);
   });
 });
