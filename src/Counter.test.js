d3ee90d29c62d821bed5de730501aa278ffcd35d
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-21 15:13:10 +0200

    Test if count should increment be 1 if the increment button is clicked

--- src/Counter.test.js
+++ src/Counter.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { Counter } from './Counter';
 
 describe(Counter, () => {
@@ -6,5 +6,14 @@ describe(Counter, () => {
     const { getByTestId } = render(<Counter initialCount={0} />);
     const countValue = Number(getByTestId('count').textContent);
     expect(countValue).toEqual(0);
+  });
+  it('count should increment by 1 if the increment button is clicked', () => {
+    const { getByTestId, getByRole } = render(<Counter initialCount={0} />);
+    const incrementBtn = getByRole('button', { name: 'Increment' });
+    const countValue1 = Number(getByTestId('count').textContent);
+    expect(countValue1).toEqual(0);
+    fireEvent.click(incrementBtn);
+    const countValue2 = Number(getByTestId('count').textContent);
+    expect(countValue2).toEqual(1);
   });
 });
